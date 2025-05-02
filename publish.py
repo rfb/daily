f323c1a49363b8e4dda0827165bc87c2de8cd255
Ryan Barber <rfb@skyscraper.nu>
2025-05-02 10:14:28 -0700

    metadata

--- publish.py
+++ publish.py
@@ -3,6 +3,8 @@ from template import template
 from zoneinfo import ZoneInfo
 from datetime import datetime
 from dotenv import load_dotenv
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
 
 OUTPUT_FILE = "output.mp3"
 
@@ -33,8 +35,16 @@ def web(date, filebase):
     artifact("index.html",
              template("index.html.jinja").render(args))
 
+def tag(filename, date):
+    file = MP3(filename, ID3=EasyID3)
+    file["title"] = f"ShedFM Daily for {date.strftime("%Y-%m-%d")}"
+    file["artist"] = "ShedFM"
+    file["date"] = date.strftime("%Y-%m-%d")
+    file.save()
+
 if __name__ == "__main__":
     date = datetime.now(tz)
+    tag(OUTPUT_FILE, date)
     filebase = "shedfm-daily-%s" % date.strftime("%Y-%m-%d")
     shutil.copyfile(OUTPUT_FILE, "%s.mp3" % filebase)
     os.system(f"ffmpeg -y -hide_banner -loglevel error -i {OUTPUT_FILE} -codec:a aac {filebase}.aac")
