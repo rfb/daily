18c277117e2f28e31e9e4660d15670760e8cdb55
Ryan Barber <rfb@skyscraper.nu>
2025-05-06 21:14:30 -0700

    better

--- schedule.py
+++ schedule.py
@@ -21,8 +21,17 @@ STATION_ID = os.environ.get("AZURA_STATION_ID")
 API_KEY = os.environ.get("AZURA_API_KEY")
 PLAYLIST_ID = os.environ.get("PLAYLIST_ID")
 
-files = { filename: open(filename, 'rb') }
+files = { "file": (filename, open(filename, 'rb'), 'audio/mpeg') }
 headers = { "X-API-Key": API_KEY }
+
+print("uploading %s" % filename)
+
+requests.post(
+    API_BASE + "/station/%s/files/upload" % STATION_ID,
+    headers=headers,
+    files=files).raise_for_status()
+
+print("searching for daily news files")
 
 response = requests.get(
     API_BASE + "/station/%s/files" % STATION_ID,
@@ -32,8 +41,10 @@ response.raise_for_status()
 records = response.json()
 
 for rec in filter(lambda rec: re.match("shedfm-daily", rec["path"]), records):
-    payload = {
-        "playlists": [ { "id": PLAYLIST_ID } ] if rec["path"] == filename else []    }
+    payload = {"playlists": [ { "id": PLAYLIST_ID } ] if rec["path"] == filename else []}
+
+    print("setting playlists on %s to %s", (rec["path"], json.dumps(payload)))
+
     requests.put(
         API_BASE + "/station/%s/file/%d" % (STATION_ID, rec["id"]),
         headers=headers | { "content-type": "application/json" },
